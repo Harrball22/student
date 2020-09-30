0f1b7a817030cd4e88ca67b1b8c662a71a202fc5
Harrison Dutson <totodutson@gmail.com>
2020-09-30 08:24:15 -0600

    Added About Me Project

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Harrison Dutson on 9/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
